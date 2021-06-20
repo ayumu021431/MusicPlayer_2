52be761a24ef66a8caa904ef1d03df73aad310c7
ayumu.sora <ayumu_skywalker@icloud.com>
2021-06-21 02:33:03 +0900

    Initial Commit

---
+++ MusicPlayer_2Tests/MusicPlayer_2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  MusicPlayer_2Tests.swift
+//  MusicPlayer_2Tests
+//
+//  Created by asora on 2021/06/21.
+//
+
+import XCTest
+@testable import MusicPlayer_2
+
+class MusicPlayer_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
